3e1362a89f09bf931c94d4de093ce8f5f7c5aef2
Rafael Sergio <rafa.sergio.02@live.com>
2020-06-03 21:33:06 -0300

    finalizado api da Ecoleta

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(cors());
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333, () => {
+  console.log('App started');
+});
